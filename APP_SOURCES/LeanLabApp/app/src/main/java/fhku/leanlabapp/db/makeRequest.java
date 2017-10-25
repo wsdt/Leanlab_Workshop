f7a9a749c8927f9ccf210e7c17fb415c562b8587
kevin <kevin.riedl.privat@gmail.com>
2017-10-25 10:59:55 +0200

    added code https

--- APP_SOURCES/LeanLabApp/app/src/main/java/fhku/leanlabapp/db/makeRequest.java
+++ APP_SOURCES/LeanLabApp/app/src/main/java/fhku/leanlabapp/db/makeRequest.java
@@ -63,6 +63,40 @@ public class makeRequest {
             }
         } else {
             //USE HTTPS
+            //TODO: Untested and not configured
+            String httpsURL = "https://www.abcd.com/auth/login/";
+
+            String query = "email="+URLEncoder.encode("abc@xyz.com","UTF-8");
+            query += "&";
+            query += "password="+URLEncoder.encode("abcd","UTF-8") ;
+
+            URL myurl = new URL(httpsURL);
+            HttpsURLConnection con = (HttpsURLConnection)myurl.openConnection();
+            con.setRequestMethod("POST");
+
+            con.setRequestProperty("Content-length", String.valueOf(query.length()));
+            con.setRequestProperty("Content-Type","application/x-www- form-urlencoded");
+            con.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0;Windows98;DigExt)");
+            con.setDoOutput(true);
+            con.setDoInput(true);
+
+            DataOutputStream output = new DataOutputStream(con.getOutputStream());
+
+
+            output.writeBytes(query);
+
+            output.close();
+
+            DataInputStream input = new DataInputStream( con.getInputStream() );
+
+
+
+            for( int c = input.read(); c != -1; c = input.read() )
+                System.out.print( (char)c );
+            input.close();
+
+            System.out.println("Resp Code:"+con .getResponseCode());
+            System.out.println("Resp Message:"+ con .getResponseMessage());
         }
     }
 }
