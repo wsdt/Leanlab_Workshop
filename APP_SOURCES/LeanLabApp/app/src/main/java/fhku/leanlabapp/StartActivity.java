6c479c9abc0df3c6c0df3e25ee085a6d75641a45
Gratt Martin <1610653951@stud.fh-kufstein.ac.at>
2017-10-30 11:46:43 +0100

    hallo

--- APP_SOURCES/LeanLabApp/app/src/main/java/fhku/leanlabapp/StartActivity.java
+++ APP_SOURCES/LeanLabApp/app/src/main/java/fhku/leanlabapp/StartActivity.java
@@ -1,8 +1,17 @@
 package fhku.leanlabapp;
 
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
 /**
  * Created by Jojo on 24.10.2017.
  */
 
-public class StartActivity {
+public class StartActivity extends AppCompatActivity {
+
+    public void openQrScanner(View view){
+        Toast.makeText(this, "1", Toast.LENGTH_SHORT).show();
+    }
+
 }
