3248bcc149814037ec285d128e3eba783533bd5e
kevin <kevin.riedl.privat@gmail.com>
2017-10-31 15:39:48 +0100

    gradle update

--- APP_SOURCES/LeanLabApp/app/src/main/java/fhku/leanlabapp/interfaces/Mapper.java
+++ APP_SOURCES/LeanLabApp/app/src/main/java/fhku/leanlabapp/interfaces/Mapper.java
@@ -1,5 +1,6 @@
 package fhku.leanlabapp.interfaces;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.json.JSONObject;
@@ -10,20 +11,20 @@ import java.io.IOException;
 public class Mapper extends ObjectMapper {
     /* Maps e.g. a Mysql object/JSON object/String into a Java class or other types. */
 
-    public Object JsonObjtoObj(JSONObject jsonObj) {
-        return null;
+    public Object JsonObjtoObj(JSONObject jsonObj, Object obj) throws IOException {
+        return this.JsonStrToObj(this.JsonObjToJsonStr(jsonObj),obj);
     }
 
-    public JSONObject ObjToJsonObj(Object genObj) {
-        return null;
+    public JSONObject ObjToJsonObj(Object obj) throws JsonProcessingException {
+        return (new JsonStrConverter(this.ObjToJsonStr(obj))).convertStrToJson();
     }
 
-    public Object JsonStrtoObj(String jsonStr, Object obj) throws IOException {
+    public Object JsonStrToObj(String jsonStr, Object obj) throws IOException {
         return this.readValue(jsonStr, obj.getClass());
     }
 
-    public String ObjtoJsonStr(Object genObj) {
-        return null;
+    public String ObjToJsonStr(Object obj) throws JsonProcessingException {
+        return this.writeValueAsString(obj);
     }
 
     public String JsonObjToJsonStr(JSONObject jsonObj) { //Dummy Method, if someone wants to convert a String to Json with the Mapper
