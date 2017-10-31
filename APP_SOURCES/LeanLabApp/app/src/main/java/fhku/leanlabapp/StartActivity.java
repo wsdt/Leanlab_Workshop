297df0104292fa61b368fe532ad78da44517e1b9
gitsani <stud.alexandra.kaltschmid@fh-kufstein.ac.at>
2017-10-31 14:56:52 +0100

    changes in startactivity and qr activity

--- APP_SOURCES/LeanLabApp/app/src/main/java/fhku/leanlabapp/StartActivity.java
+++ APP_SOURCES/LeanLabApp/app/src/main/java/fhku/leanlabapp/StartActivity.java
@@ -1,12 +1,14 @@
 package fhku.leanlabapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 /**
- * Created by Jojo on 24.10.2017.
+ * Created not by Jojo on 24.10.2017.
  */
 
 public class StartActivity extends AppCompatActivity {
@@ -15,10 +17,19 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        Button b = (Button) findViewById(R.id.buttonqr1);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this, QrActivity.class));
+            }
+        });
+
     }
 
-    public void openQrScanner(View view){
-        Toast.makeText(this, "1", Toast.LENGTH_SHORT).show();
+
+
+
     }
 
-}
+
