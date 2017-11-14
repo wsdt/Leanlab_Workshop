30b0aa3f9dab06728ff286c4e2a00ccb65df8569
martingratt <martin_gratt@hotmail.com>
2017-11-14 14:45:59 +0100

    spinners startactivity

--- APP_SOURCES/LeanLabApp/app/src/main/java/fhku/leanlabapp/StartActivity.java
+++ APP_SOURCES/LeanLabApp/app/src/main/java/fhku/leanlabapp/StartActivity.java
@@ -4,17 +4,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.TextView;
 
 
+public class StartActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, AdapterView.OnItemSelectedListener {
 
-public class StartActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        String [] stations = bringMyStations();
+        String [] prodcuts = bringMyProducts();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
@@ -26,53 +31,62 @@ public class StartActivity extends AppCompatActivity {
             }
         });
 
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        //Getting the instance of Spinner and applying OnItemSelectedListener on it
+        Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
+        spinner1.setOnItemSelectedListener(this);
+
+        //Creating the ArrayAdapter instance having the country list
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,stations);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        spinner1.setAdapter(aa);
+
+
+        Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
+        spinner2.setOnItemSelectedListener(this);
+        ArrayAdapter bb = new ArrayAdapter(this,android.R.layout.simple_spinner_item,prodcuts);
+        bb.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        spinner2.setAdapter(bb);
 
 
 
-        String [] arraySpinner = new String[] {
-                "1", "2", "3", "4", "5"
-        };
+        Intent intent = getIntent();
+
+        String qrcode = intent.getStringExtra(QrActivity.EXTRA_Message);
+
+        spinner1.setSelection(aa.getPosition(qrcode));
+
+    }
 
 
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position,long id) {
 
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                    android.R.layout.simple_spinner_item, arraySpinner);
-            spinner.setAdapter(adapter);
+    }
 
-            Intent intent = getIntent();
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
 
-            String qrcode = intent.getStringExtra(QrActivity.EXTRA_Message);
-
-
-
-            TextView textView = (TextView) findViewById(R.id.qrcode);
-            textView.setText(qrcode);
-
-
-
-            /*String json = "{\"productid\" : \"1\",\"productname\":\"testname\"}";
-        Product tmp = new Product();
-        try {
-            /*Module temp = tmp.getIgnoreJacksonModule();
-            Log.e("IOExc","ModuleString:"+temp.toString());
-            tmp = (Product) tmp.JsonStrToObj(json,tmp);
-            Log.e("NOExc",tmp.getProductid()+"\nName: "+tmp.getProductname());*
-            JSONObject jsonobj=tmp.JsonStrToJsonObj(json);
-            Log.e("SUCCESS",jsonobj.get("Productid").toString());
-
-            json = tmp.JsonObjToJsonStr(jsonobj);
-            Log.e("SUCCESS","JSON STRING: "+json);
-        } catch (Exception e) {
-            Log.e("IOExc","ERROR: "+e.getMessage());
-            for (StackTraceElement m : e.getStackTrace()) {
-                Log.e("IOExc",m.toString());
-            }
-        }*/
 
     }
 
 
 
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    public String [] bringMyStations(){
+        String [] stations = {"Station1", "Station2", "Station3", "Station4"};
+        return stations;
+    }
+
+    public String [] bringMyProducts(){
+        String [] products = {"Product1", "Product2", "Product3", "Product4"};
+        return products;
+    }
 
 }
